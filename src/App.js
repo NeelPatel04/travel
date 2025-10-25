cd54f29a43e9cf7f2f88c30b60e90e30c1cdce73
Neel Patel <neelpp@email.sc.edu>
2025-10-25 18:01:42 -0400

    ready deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Destination from './pages/Destination';
@@ -11,17 +11,15 @@ import Preview from './pages/Preview';
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/destination" element={<Destination />} />
-          <Route path="/blog" element={<TravelBlog />} />
-          <Route path="/bucket-list" element={<BucketList />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/preview" element={<Preview />} />
-        </Routes>
-      </Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/blog" element={<TravelBlog />} />
+        <Route path="/bucket-list" element={<BucketList />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/preview" element={<Preview />} />
+      </Routes>
     </BrowserRouter>
   );
 }
